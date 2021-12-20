48ede899368fd36792cd8540e896794b72b3cd12
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2021-12-20 15:52:51 +0300

    database structure

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require("express");
- require('dotenv').config();
+require('dotenv').config();
+const app = express();
+const db = require("./db");
 const morgan = require("morgan");
 const cors = require("cors");
 
-
-
- const app = express();
-
  const PORT= process.env.PORT || 5000;
 
-
+/// app-level middleware 
+app.use(express.json());
 
  app.listen(PORT, () => {
    console.log(`Server Started On : ${PORT}`);
