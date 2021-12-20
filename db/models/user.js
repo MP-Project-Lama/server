54bdf25c247a58db3211210beb757cdb47774c2f
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2021-12-20 16:05:18 +0300

    create user schema

---
+++ db/models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+
+
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  isDel: { type: Boolean, default: false },
+
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    default:
+      "https://www.kindpng.com/picc/m/24-248253_user-profile-default-image-png-clipart-png-download.png",
+  },
+  resetCode: {
+    type: String,
+  },
+  activeCode: {
+    type: String,
+  },
+  isActive: {
+    type: Boolean,
+    default: false,
+  },
+  role: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Role",
+    default: "",
+  },
+});
+
+
+
+module.exports = mongoose.model("User", userSchema);
