9ac1140de700e2362e915a6c83e3db0b3a8aa6f4
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2021-12-20 16:24:21 +0300

    create like schema

---
+++ db/models/like.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const likeSchema = new mongoose.Schema({
+  isLike: {
+    type: Boolean,
+    default: false,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  collection: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Collection",
+  },
+});
+
+module.exports = mongoose.model("Like", likeSchema);
