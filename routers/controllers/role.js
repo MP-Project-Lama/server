6053e26df7759c76b717ec2eb2bc1a7c77bd96dd
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2021-12-20 16:56:35 +0300

    role routers : get all roles router

--- routers/controllers/role.js
+++ routers/controllers/role.js
@@ -1,5 +1,7 @@
 const roleModel = require("./../../db/models/role");
 
+
+// create a new role in the app by this function : 
 const createRole = (req, res) => {
   const { role } = req.body;
 
@@ -17,4 +19,15 @@ const createRole = (req, res) => {
     });
 };
 
-module.exports = { createRole };
+/// get all roles by this function : 
+const getRoles = (req, res) => {
+  roleModel
+    .find({})
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
+module.exports = { createRole, getRoles };
--- routers/routes/role.js
+++ routers/routes/role.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const rolesRouter = express.Router();
 
-const { createRole } = require("../controllers/role");
+const { createRole, getRoles } = require("../controllers/role");
 
 rolesRouter.post("/role", createRole);
+rolesRouter.get("/roles", getRoles);
 
 
 module.exports = rolesRouter;
